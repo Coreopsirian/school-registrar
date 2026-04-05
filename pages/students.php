074b27f640f080ec7efecba94beaf54e0bc783ea
novadar-star <jolynekujoismywife06@gmail.com>
2026-04-05 16:55:02 +0800

    included bootsrap js cdn

--- pages/students.php
+++ pages/students.php
@@ -68,7 +68,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link rel="icon" type="image/png" href="../images/COJ.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
+  <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
   <link rel="stylesheet" href="../css/styles.css">
   <link rel="stylesheet" href="../css/students.css">
